a9e852d94540e7c2addc07e09a7e5a9523cad5f7
Condzi <Condzi@users.noreply.github.com>
2018-07-27 17:32:19 +0200

    fix timer fading again

--- Game/Inc/GamePCH.hpp
+++ Game/Inc/GamePCH.hpp
@@ -75,22 +75,39 @@ struct CurrentTurn final
 
 struct Timer final
 {
+	Timer()
+	{
+		auto desiredColor = TextColorCounting;
+		auto startingColor = TextColorCountdown;
+		auto t = CountdownTime.asSeconds();
+
+		// just V = s/t
+		colorDelta.r = ( desiredColor.r - startingColor.r ) / t;
+		colorDelta.g = ( desiredColor.g - startingColor.g ) / t;
+		colorDelta.b = ( desiredColor.b - startingColor.b ) / t;
+		colorDelta.a = ( desiredColor.a - startingColor.a ) / t;
+	}
+
 	enum class Mode
 	{
 		Countdown,
 		Counting
 	} mode = Mode::Countdown;
 
-	sf::Time countdownTime = sf::seconds( 1 );
+	inline static const sf::Time CountdownTime = sf::seconds( 2 );
 	sf::Clock clock;
 	con::Text text;
 	inline static const sf::Color TextColorCountdown = sf::Color::Red;
 	inline static const sf::Color TextColorCounting = sf::Color::White;
+	struct
+	{
+		float32_t r, g, b, a;
+	} colorDelta;
 	// @ToDo: Different text color for Modes.
 
 	void reset()
 	{
-		auto countdownTimeStr = con::ConvertTo<std::string>( countdownTime.asSeconds() );
+		auto countdownTimeStr = con::ConvertTo<std::string>( CountdownTime.asSeconds() );
 
 		text.setString( con::ConvertTo<std::string>( "-", countdownTimeStr.substr( 0, countdownTimeStr.find( '.' ) + 3 ), "s" ) );
 		clock.restart();
@@ -102,9 +119,9 @@ struct Timer final
 	{
 		std::string secondsString;
 		if ( mode == Mode::Countdown ) {
-			float sec = clock.getElapsedTime().asSeconds();
-			float countdownSec = countdownTime.asSeconds();
-			float countdownDelta = countdownSec - sec;
+			float32_t sec = clock.getElapsedTime().asSeconds();
+			float32_t countdownSec = CountdownTime.asSeconds();
+			float32_t countdownDelta = countdownSec - sec;
 
 			// @Bug: Can't set sf::Clock time point, so there is small amout of milliseconds that are discarded.
 			if ( countdownDelta <= 0.0f ) {
@@ -114,10 +131,12 @@ struct Timer final
 			} else {
 				// Fading to TextColorCounting 
 				auto currentColor = text.getFillColor();
-				const auto desiredColor = TextColorCounting;
-				currentColor.r += ( desiredColor.r - currentColor.r ) * ( countdownSec - countdownDelta );
-				currentColor.g += ( desiredColor.g - currentColor.g ) * ( countdownSec - countdownDelta );
-				currentColor.b += ( desiredColor.b - currentColor.b ) * ( countdownSec - countdownDelta );
+				auto dt = con::Global.FrameTime.asSeconds();
+
+				currentColor.r += colorDelta.r * dt;
+				currentColor.g += colorDelta.g * dt;
+				currentColor.b += colorDelta.b * dt;
+				currentColor.a += colorDelta.a * dt;
 
 				std::cout << (int)currentColor.r << ", " << (int)currentColor.g << ", " << (int)currentColor.b << ", d: "<< countdownDelta << '\n';
 
